3a908d2c9ebac18216403bff619ccdaa2835f860
Richard Hayes <richardhayes777@gmail.com>
2015-07-26 18:11:27 +0100

    Added note

---
+++ CardGameProject/app/src/main/java/griffhaze/co/uk/cardgameproject/model/card/Card.java
@@ -0,0 +1,13 @@
+package griffhaze.co.uk.cardgameproject.model.card;
+
+/**
+ * Created by richardhayes on 26/07/15.
+ */
+public abstract class Card {
+
+    private final String name;
+
+    public Card(String name) {
+        this.name = name;
+    }
+}
